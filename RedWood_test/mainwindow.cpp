f5be4a474eb30b2d45ca5175154d0648b09d868d
BatleCat <84068158+BatleCat@users.noreply.github.com>
2021-05-23 00:18:33 +0300

    Вывод на форму изображение яблока

--- RedWood_test/mainwindow.cpp
+++ RedWood_test/mainwindow.cpp
@@ -3,6 +3,10 @@
 #include "ui_mainwindow.h"
 #include <QPainter>
 #include <QImage>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QColor>
+#include <QtDebug>
 //-----------------------------------------------------------------------------
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,11 +17,32 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     apple.set_predmet_type(GREEN_APPLE);
-    apple.set_predmet_img(QString::fromUtf8("c:/green_apple.jpg"));
+    apple.set_predmet_img(QString::fromUtf8(":/img/green-apple.jpg"));
 
-    QPainter painter(ui->widget_predmet);
-    QImage   img(apple.get_predmet_img());
-    painter.drawImage(0,0,img);
+    QImage   img;
+//    if ( !img.load( apple.get_predmet_img() ) )
+//    {
+//        qDebug() << "Error load JPG-file!";
+//    }
+    img.load(apple.get_predmet_img(), "JPG");
+
+    QImage img1 = img.scaled( ui->widget_predmet->size(), Qt::KeepAspectRatio );
+
+    QHBoxLayout* phbx = new QHBoxLayout;
+    phbx->setMargin(0);
+    phbx->setSpacing(0);
+
+    QLabel* plbl = new QLabel;
+    plbl->setFixedSize(img1.size());
+    plbl->setPixmap(QPixmap::fromImage(img1));
+
+//    QPixmap pix_map(apple.get_predmet_img());
+//    plbl->setFixedSize(pix_map.size());
+//    plbl->setPixmap(pix_map);
+
+    phbx->addWidget(plbl);
+//    ui->widget_predmet->setFixedSize(img.size());
+    ui->widget_predmet->setLayout(phbx);
 }
 //-----------------------------------------------------------------------------
 MainWindow::~MainWindow()
